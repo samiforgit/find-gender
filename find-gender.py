32f2b7fc524d8c22e2800de88c428f1b1d6bb1c6
samiforgit <mdsamiul05@gmail.com>
2016-10-08 00:00:19 +0530

    Create find-gender.py

---
+++ find-gender.py
@@ -0,0 +1,12 @@
+import json
+import urllib2
+
+Key = "find-your-gender"
+name = raw_input("Enter name : ");
+if name:
+    data = json.load(urllib2.urlopen("https://gender-api.com/get?key=" + Key + "&name="+name))
+    print "Gender: " + data["gender"];
+else:
+    print "Error!!"
+
+#i.e Gender: male
